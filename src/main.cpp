70be9bc74cba895c206cf3483d324a6bcac1bb5b
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2020-12-29 16:53:52 -0800

    allow remote connections

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <DNSServer.h>  //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>  //Local WebServer used to serve the configuration portal
-#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <TaskScheduler.h>
 #include <WiFiClientSecure.h>
@@ -221,4 +220,5 @@ void loop() {
     }
   }
   server.handleClient();
+  ArduinoOTA.handle();
 }
